9b7099c98855a8825cb6037a8fd3f924cd6bd5de
Haroon Iqbal <haroonsiqbal@gmail.com>
2019-12-09 10:50:30 -0600

    config settings

--- FireSafe/Startup.cs
+++ FireSafe/Startup.cs
@@ -12,6 +12,7 @@ using FireSafe.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using FireSafe.Models;
 
 namespace FireSafe
 {
@@ -30,7 +31,7 @@ namespace FireSafe
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
