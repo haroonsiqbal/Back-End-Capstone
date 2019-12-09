eb171164b4a5a25e91359a4e605187ddb0167111
Haroon Iqbal <haroonsiqbal@gmail.com>
2019-12-09 10:39:11 -0600

    set up models

--- FireSafe/Data/ApplicationDbContext.cs
+++ FireSafe/Data/ApplicationDbContext.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FireSafe.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FireSafe.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<Log> Logs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+
     }
 }
---
+++ FireSafe/Models/Log.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FireSafe.Models
+{
+    public class Log
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Manufacturer { get; set; }
+        [Required]
+        public string Model { get; set; }
+
+        [Required(ErrorMessage = "Please choose a category.")]
+        [Display(Name = "Product Category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        [Required]
+        public double Price { get; set; }
+        
+        [Required(ErrorMessage = "Please choose a seller.")]
+        [Display(Name = "Purchased From")]
+        public int SellerId { get; set; }
+        public Seller Seller { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        
+        [Required]
+        [Display(Name = "Date Purchased")]
+        [DataType(DataType.Date)]
+        public DateTime PurchaseDate { get; set; }
+        public string Comment { get; set; }
+        
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public ApplicationUser User { get; set; }
+    }
+}
